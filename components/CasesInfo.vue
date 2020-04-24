bd22995a61f17e474986fe751a5ac7c9dd78d559
Luis Pereira <luis.pereira.moz@gmail.com>
2020-04-24 19:56:13 +0200

    Removed Calendar

--- components/CasesInfo.vue
+++ components/CasesInfo.vue
@@ -1,28 +1,16 @@
 <template>
   <div class="bg-brown flex flex-wrap flex-col">
+    <h2 class="text-gold font-semibold">Progresso dos Dados</h2>
     <div class="w-full">
-      <h3 class="text-white">Situação Moçambique</h3>
-    </div>
-    <div class="w-full mt-2">
-      <p class="text-white">
-        Actualizado à: <span class="text-gold">16h20 – 20/04/2020</span>
-      </p>
-    </div>
-    <div class="w-full my-5">
-      <calendar></calendar>
-    </div>
-    <div class="w-full">
-      <chart />
+      <Chart class="pt-4" />
     </div>
   </div>
 </template>
 
 <script>
-import Calendar from '~/components/Calendar'
 import Chart from '~/components/Chart'
 export default {
   components: {
-    Calendar,
     Chart
   }
 }
