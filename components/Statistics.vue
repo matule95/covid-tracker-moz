a1cf9949efd148de8895badd682faa8aa703beab
Luis PEreira <luis.pereira.moz@gmail.com>
2021-02-12 20:42:24 +0200

    fix: fix statistics breakpoints

--- components/Statistics.vue
+++ components/Statistics.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="w-full flex flex-wrap flex-1">
-    <div class="xxs:w-full xs:w-1/2 md:w-1/4 xl:w-1/5 xxl:w-1/6">
+    <div class="xxs:w-full md:w-1/2 xl:w-1/5 xxl:w-1/6 ">
       <stats-counter
         :stats="{
           type: 'countries',
@@ -28,9 +28,7 @@
         </template>
       </stats-counter>
     </div>
-    <div
-      class="xxs:w-full xs:w-1/2 md:w-1/4 xl:w-1/5 xxl:w-1/6 xxs:mt-5 xs:mt-0"
-    >
+    <div class="xxs:w-full md:w-1/2 xl:w-1/5 xxl:w-1/6 xxs:mt-5 md:mt-0">
       <stats-counter
         :stats="{
           type: 'infected',
@@ -59,7 +57,7 @@
         </template>
       </stats-counter>
     </div>
-    <div class="xxs:w-full xs:w-1/2 md:w-1/4 xl:w-1/5 xxl:w-1/6 mt-5 md:mt-0">
+    <div class="xxs:w-full md:w-1/2 xl:w-1/5 xxl:w-1/6 mt-5 xl:mt-0">
       <stats-counter
         :stats="{
           type: 'active',
@@ -105,7 +103,7 @@
         </template>
       </stats-counter>
     </div>
-    <div class="xxs:w-full xs:w-1/2 md:w-1/4 xl:w-1/5 xxl:w-1/6 mt-5 xl:mt-0">
+    <div class="xxs:w-full md:w-1/2 xl:w-1/5 xxl:w-1/6 mt-5 xl:mt-0">
       <stats-counter
         :stats="{
           type: 'recovered',
@@ -133,7 +131,7 @@
         </template>
       </stats-counter>
     </div>
-    <div class="xxs:w-full xs:w-1/2 md:w-1/4 xl:w-1/5 xxl:w-1/6 mt-5 md:mt-0">
+    <div class="xxs:w-full md:w-1/2 xl:w-1/5 xxl:w-1/6 mt-5 xl:mt-0">
       <stats-counter
         :stats="{
           type: 'deaths',
@@ -171,7 +169,7 @@
         </template>
       </stats-counter>
     </div>
-    <div class="xxs:w-full xs:w-1/2 md:w-1/4 xl:w-1/5 xxl:w-1/6 mt-5 xxl:mt-0">
+    <div class="xxs:w-full md:w-1/2 xl:w-1/5 xxl:w-1/6 mt-5 xxl:mt-0">
       <StatsCounter
         :stats="{
           type: 'hospitalized',
@@ -200,7 +198,7 @@
         </template>
       </StatsCounter>
     </div>
-    <div class="xxs:w-full xs:w-1/2 md:w-1/4 xl:w-1/5 xxl:w-1/6 mt-5 xxl:mt-0">
+    <div class="xxs:w-full md:w-1/2 xl:w-1/5 xxl:w-1/6 mt-5 xxl:mt-0">
       <StatsCounter
         :stats="{
           type: 'local_transmissions',
@@ -229,7 +227,7 @@
         </template>
       </StatsCounter>
     </div>
-    <div class="xxs:w-full xs:w-1/2 md:w-1/4 xl:w-1/5 xxl:w-1/6 mt-5">
+    <div class="xxs:w-full md:w-1/2 xl:w-1/5 xxl:w-1/6 mt-5">
       <StatsCounter
         :stats="{
           type: 'foreign_transmission',
