0743f987962f0cd043d71d05752b8caf8e156406
Clayton Matule <clayton.matule@hotmail.com>
2021-07-08 18:57:12 +0200

    feat: added year filter to TestedPositive chart

--- components/Partials/ChartsSection.vue
+++ components/Partials/ChartsSection.vue
@@ -14,8 +14,15 @@
         >Cumulativo de
         <span class="text-gold">Testados e Positivos</span> Mensalmente</span
       >
+      <div class="w-full flex flex-end">
+        <select-input
+          :options="monthlyInformation.years"
+          v-model="selectedYear"
+          label="Escolha o ano"
+        />
+      </div>
       <TestedAndPositiveChart
-        :chart-data="monthlyDistribution"
+        :chart-data="monthlyInformation"
         class="justify-left w-full pt-10"
       />
     </div>
@@ -38,18 +45,21 @@
 </template>
 
 <script>
+import SelectInput from '../SelectInput'
 import AgeRangeChart from '~/components/Partials/AgeRangeChart'
 import TestedAndPositiveChart from '~/components/Partials/TestedAndPositiveChart'
 import WeeklyCasesChart from '~/components/Partials/WeeklyCasesChart'
 export default {
   components: {
+    SelectInput,
     AgeRangeChart,
     TestedAndPositiveChart,
     WeeklyCasesChart
   },
   data: () => ({
     genders: ['Masculino', 'Feminino'],
-    origins: ['Moçambicana', 'Outras']
+    origins: ['Moçambicana', 'Outras'],
+    selectedYear: 2020
   }),
   computed: {
     weeklyInformation() {
@@ -95,6 +105,64 @@ export default {
         chartData.data.push(item.cases)
       })
       return chartData
+    },
+    monthlyInformation() {
+      const data = this.$store.state.statistics.dailyInformation.map(
+        (dailyInfo, index) => {
+          const stats = this.$store.state.statistics.dailyInformation
+          const daily = {}
+          const yesterday =
+            // eslint-disable-next-line camelcase
+            // eslint-disable-next-line camelcase
+            stats[index + 1]?.country_stats || 0
+          daily.date = dailyInfo.date
+          daily.tested =
+            parseInt(dailyInfo.country_stats.tested) -
+            parseInt(yesterday?.tested)
+          daily.infected = parseInt(dailyInfo.country_stats.infected)
+          daily.cases =
+            parseInt(dailyInfo.country_stats.infected) -
+            parseInt(yesterday?.infected)
+          return daily
+        }
+      )
+      const monthlyData = []
+      data.reverse().forEach(daily => {
+        const date = daily.date.split('/')
+        const newDate = date[1] + '/' + date[0] + '/' + date[2]
+        const month = new Date(newDate).getMonth()
+        const year = new Date(newDate).getFullYear()
+        const monthName = new Intl.DateTimeFormat('pt', {
+          month: 'short'
+        }).format(new Date(newDate))
+        const index = monthlyData.findIndex(
+          mt => mt.month === month && mt.year === year
+        )
+        if (index !== -1) {
+          monthlyData[index].tested += daily.tested || 0
+          monthlyData[index].infected += daily.cases || 0
+        } else {
+          monthlyData.push({
+            month,
+            monthName,
+            year,
+            tested: daily.tested || 0,
+            infected: daily.cases || 0
+          })
+        }
+      })
+      return {
+        cases: monthlyData
+          .filter(md => md.year === this.selectedYear)
+          .map(md => md.infected),
+        tested: monthlyData
+          .filter(md => md.year === this.selectedYear)
+          .map(md => md.tested),
+        labels: monthlyData
+          .filter(md => md.year === this.selectedYear)
+          .map(md => md.monthName),
+        years: Array.from(new Set(monthlyData.map(md => md.year)))
+      }
     }
   }
 }
