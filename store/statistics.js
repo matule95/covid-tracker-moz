e77c656e9bf142d82d432c363d62dcf45a5235ba
Clayton Matule <clayton.matule@hotmail.com>
2024-01-04 15:18:40 +0200

    wip: add local json

--- store/statistics.js
+++ store/statistics.js
@@ -1,17 +1,24 @@
+import payload from '../static/data/finalPayload.json'
 export const state = () => ({
   all: []
 })
 export const actions = {
   fetchAll({ commit }) {
-    return this.$axios
-      .get('/getData')
-      .then(({ data }) => {
-        commit('SET_ITEMS', {
-          key: 'all',
-          items: data
-        })
-      })
-      .catch(error => Promise.reject(error))
+    // read JSON file
+    const data = payload
+    commit('SET_ITEMS', {
+      key: 'all',
+      items: data
+    })
+    // return this.$axios
+    //   .get('/getData')
+    //   .then(({ data }) => {
+    //     commit('SET_ITEMS', {
+    //       key: 'all',
+    //       items: data
+    //     })
+    //   })
+    //   .catch(error => Promise.reject(error))
   }
 }
 export const mutations = {
